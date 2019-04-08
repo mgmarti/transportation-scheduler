5cd2886651f254da81f5b1a3a52247134e1e97f4
moma <mogimartinez@gmail.com>
2019-04-08 14:42:13 -0500

    style.css added and index.html updated to have form and table

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyB0AO_D_Yz46kuXbp32LLAvC-ZE_Iy0UTI",
+    authDomain: "scheduler-c546c.firebaseapp.com",
+    databaseURL: "https://scheduler-c546c.firebaseio.com",
+    projectId: "scheduler-c546c",
+    storageBucket: "scheduler-c546c.appspot.com",
+    messagingSenderId: "93316995768"
+  };
+  firebase.initializeApp(config);
+
+  
