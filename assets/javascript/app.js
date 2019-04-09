6c6085aeab8709322624564bf4345971b9053523
moma <mogimartinez@gmail.com>
2019-04-08 20:10:49 -0500

    fixed id name errors in app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,47 @@
  // Initialize Firebase
  var config = {
-    apiKey: "AIzaSyB0AO_D_Yz46kuXbp32LLAvC-ZE_Iy0UTI",
-    authDomain: "scheduler-c546c.firebaseapp.com",
-    databaseURL: "https://scheduler-c546c.firebaseio.com",
-    projectId: "scheduler-c546c",
-    storageBucket: "scheduler-c546c.appspot.com",
-    messagingSenderId: "93316995768"
+    apiKey: "AIzaSyCImU1ctqpI5dgUKfSpu3sCukD7UiamRBU",
+    authDomain: "train-scheduler-d67dd.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d67dd.firebaseio.com",
+    projectId: "train-scheduler-d67dd",
+    storageBucket: "train-scheduler-d67dd.appspot.com",
+    messagingSenderId: "97855616553"
   };
   firebase.initializeApp(config);
-
+  console.log(config)
   let trainName = "";
   let trainDestination = "";
   let trainTime = "";
   let trainFrequency = "";
 
-  $("submit").on("click", function(event) {
-      event.preventDefault();
+  $("#submit").on("click", function(event){
+    event.preventDefault();
 
-    trainName = $("#trainNameDisplay").val().trim();
-    trainDestination = $("#destinationDisplay").val().trim();
-    trainFrequency = $("#frequencyDisplay").val().trim();
-   
+  trainName = $("#train-name").val().trim();
+  trainDestination = $("#train-destination").val().trim();
+  trainFrequency = $("#train-frequency").val().trim();
 
-    firebase.database().ref().push({
-        trainName: trainName,
-        trainDestination: trainDestination,
-        trainFrequency: trainFrequency,
-        trainTime: trainTime
-      });
-  })
+  firebase.database().ref().push({
+    trainName: trainName,
+    trainDestination: trainDestination,
+    trainFrequency: trainFrequency,
+    });
+
+})
+
+firebase.database().ref().on("value", function(snapshot){
+    console.log(snapshot.val());
+  $("#trainNameDisplay").html(snapshot.val().trainName);
+  $("#destinationDisplay").html(snapshot.val().trainDestination);
+  $("#frequencyDisplay").html(snapshot.val().trainFrequency);
+  $("#divider").html();
+    console.log(snapshot.val());
+})
+
+
+firebase.database().ref().on("child_added", function(childSnapshot) {
+  $("#train-name-display").append("<p>" +childSnapshot.val().trainName+"</p>")
+  $("#destination-display").append("<p>" +childSnapshot.val().trainDestination+"</p>")
+  $("#frequency-display").append("<p>" +childSnapshot.val().trainFrequency+"</p>")
+    console.log(childSnapshot);
+})
